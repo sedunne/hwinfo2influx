600e2f4a8c6b0cf05e8d2451dfe922969a55917b
Stephen Dunne <stephen@icanhazmail.net>
2020-05-30 03:55:20 -0400

    updates

--- scripts/hwicsv_convert.rb
+++ scripts/hwicsv_convert.rb
@@ -9,13 +9,14 @@ require 'csv'
 require 'date'
 
 ## open output file, and process our input
-fixed = CSV.open('output', 'wb', :headers => true, :return_headers => true)
+fixed = CSV.open(ARGV[1], 'wb', :headers => true, :return_headers => true)
 
 ## load input csv and convert the headers to all lowercase
 csv = CSV.table(ARGV[0], :headers => true, :header_converters => lambda { |h| h.to_s.downcase.gsub(' ', '_')})
 headers = csv.headers
 
 ## write the headers out. ruby csv can do this natively, but I'm probably dumb and it wasn't working well so I just manually do it
+headers.delete_at((headers.length - 1)) ## last header is null
 discard_columns = ['date']
 header_row = headers.delete_if { |h| discard_columns.include?(h) }
 fixed << header_row
@@ -26,21 +27,27 @@ fixed << header_row
 csv.each_with_index do |row, i|
   ## merge date and time columns, and yeet the date column
   row['time'] = "#{row['date']} #{row['time']}"
-  row.delete('date')
-
   ## convert the time field to rfc3339 (mostly for influxdb compatibility)
   row['time'] = DateTime.strptime("#{row['time']} -0400", '%d.%m.%Y %H:%M:%S.%L %z').rfc3339
+
+  ## delete the columns that we don't want/need
+  discard_columns.each do |discard|
+    row.delete(discard)
+  end
 
   ## convert yes/no to puesdo-bool, then it and everything else into a float. there may be actual reasons to do this (eg performance of float vs int in influxdb),
   ##  but this is mostly just to heavy handedly convert everything to something useable
   row.each_with_index do |column, i|
     h,f = column[0], column[1]
-    next if h == 'time'
-    if f.to_s.match(/Yes|No/)
+    if h == 'time'
+      next
+    elsif f.to_s.match(/Yes|No/)
       row[i] = f.to_s.length == 3 ? f = 1.0 : f = 0.0
     else
       row[i] = f.to_s.to_f
     end
   end
+  ## the last header is null
+  row.delete((row.length - 1))
   fixed << row
 end
